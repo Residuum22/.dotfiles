7305ebbfd0af1a538740f7e0f8479990bdba653d
Residuum22 <mihalik.mark@gmail.com>
2026-03-01 19:10:01 +0100

    Refactor dotfiles

---
+++ .zshrc
@@ -0,0 +1,23 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# source antidote
+source ~/.antidote/antidote.zsh
+
+# initialize plugins statically with ${ZDOTDIR:-~}/.zsh_plugins.txt
+antidote load
+
+# zsh-history-substring-search configuration
+bindkey '^[[A' history-substring-search-up # or '\eOA'
+bindkey '^[[B' history-substring-search-down # or '\eOB'
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+
+alias la="ls -lA"
+alias ll="ls -l"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
--- install.sh
+++ install.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 # set -x
 
+####################################################################
+# ZSH specific part
+####################################################################
 check_zsh=$(which zsh)
 
 if [[ "$check_zsh" == *"zsh"* ]]; then
-    echo "ZSH is installed."
+    echo "ZSH is already installed."
 else
     echo "ZSH is not installed. Please install ZSH and rerun the script."
     exit 1
 fi
 
-if [[ -z ~/.oh-my-zsh ]]; then
-    echo "Oh My Zsh is not installed. Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [[ -d ~/.antidote ]]; then
+    echo "Antidote is already installed."
 else
-    echo "Oh My Zsh is already installed."
+    echo "Installing antidote..."
+    git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 fi
 
-echo "Install tmux config..."
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+echo "Copying .zsh_plugins.txt..."
+cp $(pwd)/.zsh_plugins.txt ~/.zsh_plugins.txt
 
-echo "Install zsh config..."
-ln -s $(pwd)/.zshrc ~/.zshrc
+echo "Copying .zshrc..."
+cp $(pwd)/.zshrc ~/.zshrc
 
-echo "Install .zshenv config..."
-ln -s $(pwd)/.zshenv ~/.zshenv
+echo "Copying .zshenv..."
+cp $(pwd)/.zshenv ~/.zshenv
 
-echo "Install .zprofile config..."
-ln -s $(pwd)/.zprofile ~/.zprofile
+echo "Copying .zprofile..."
+cp $(pwd)/.zprofile ~/.zprofile
 
-echo "Install tmux plugin manager..."
+echo "Copying .p10k.zsh..."
+cp $(pwd)/.p10k.zsh ~/.p10k.zsh
+####################################################################
+# TMUX specific part
+####################################################################
+echo "Copying tmux config..."
+cp $(pwd)/.tmux.conf ~/.tmux.conf
+
 if [[ -d ~/.tmux/plugins/tpm ]]; then
     echo "Tmux Plugin Manager is already installed."
 else
+    echo "Installing Tmux Plugin Manager..."
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo "Tmux Plugin Manager installed."
 fi
 
-
-
+zsh
